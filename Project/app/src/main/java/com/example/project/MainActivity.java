2408803cd9737e13a467d1d17bc877b3f72f36f3
Daniil Feoktistov <feomtistovdaniil11@gmail.com>
2026-03-15 11:40:24 +0200

    Update MainActivity.java

--- Project/app/src/main/java/com/example/project/MainActivity.java
+++ Project/app/src/main/java/com/example/project/MainActivity.java
@@ -1,24 +1,30 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnAddTask, btnViewTasks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnAddTask = findViewById(R.id.btnAddTask);
+        btnViewTasks = findViewById(R.id.btnViewTasks);
+
+        btnAddTask.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, AddEditTaskActivity.class);
+            startActivity(intent);
+        });
+
+        btnViewTasks.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, TaskListActivity.class);
+            startActivity(intent);
         });
     }
 }
